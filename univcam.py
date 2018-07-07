9a1f43c34b209829ea598cf832231d94399c7658
keita <32783655+keita-lib@users.noreply.github.com>
2018-07-07 13:11:14 +0900

    Add files via upload

---
+++ univcam.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+# This code generates the universal cam curve from cam parameters.
+
+
+# In[ ]:
+
+
+#---- input cam parameters ----
+li_t_para=[]
+li_v_para=[]
+li_s_para=[]
+
